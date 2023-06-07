4b86d17e066370cae117ecc9a4a97efaf8eb0ab5
yahyawiaf <abdelftt@gmail.com>
2023-06-07 19:24:58 +0100

    update message twillio

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,14 +4,14 @@ import { ProtectedRoute, PublicRoute } from "./ProtectedRoute.tsx";
 
 import { useAuth } from "../context/AuthProvider";
 
-import {Layout} from "../layout/Main"
+import { Layout } from "../layout/Main";
 
-//pages 
+//pages
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import Form from "../pages/Form";
 import Profile from "../pages/Profile";
-
+import Validate from "../pages/Validate";
 
 function RouteSection() {
   const { session } = useAuth();
@@ -20,7 +20,14 @@ function RouteSection() {
       <Route element={<PublicRoute session={session} />}>
         <Route path="/" element={<Login />} />
       </Route>
-      <Route element={<ProtectedRoute session={session} ><Layout /></ProtectedRoute>}>
+      <Route path="/validate/:id" element={<Validate />} />
+      <Route
+        element={
+          <ProtectedRoute session={session}>
+            <Layout />
+          </ProtectedRoute>
+        }
+      >
         <Route path="/form" element={<Form />} />
         <Route path="/form/:id" element={<Form />} />
         <Route path="/home" element={<Home />} />
